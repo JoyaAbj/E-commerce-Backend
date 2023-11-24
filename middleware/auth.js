3943bb727ce4752534e2b79ec76a2ed1f3c016d6
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-11-24 17:31:34 +0200

    modifying the authentication part

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt=require('jsonwebtoken');
 const Users=require('../models/userModel');
 const authenticated=(role)=>{
-    return (req,res,next)=>{
+    return async(req,res,next)=>{
         if (
             !req.headers.authorization ||
             !req.headers.authorization.startsWith('Bearer')
@@ -10,11 +10,13 @@ const authenticated=(role)=>{
               message: 'No auth credentials',
             });
         const {authorization} =req.headers;
-        const token=authenticated.split(' ')[1];
+        const token=authorization.split(' ')[1];
         if(!token)return res.status(400).json({message:"No token detected"});
         try{
-            const credentials=jwt.verify(token,process.env.Secret_key);
+            const credentials=jwt.verify(token,process.env.SECRET_KEY);
             if(credentials.role!==role)throw Error("You are not authorized");
+            const exist=await Users.findById({_id:credentials.Id});
+            if(!exist)throw Error("User not found !!");
             next();
         }catch(error){
             res.status(500).json({message:"An error occured",error:error.message})
