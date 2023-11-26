d203ae6cea086dcbb0f426266071226313c0aee6
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-11-26 11:11:17 +0200

    modifying models

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-  userId: { type: Schema.Types.ObjectId, ref: "users" },
-  cars: { type: Array, required: true },
-  shipmentId: {type: Schema.Types.ObjectId, ref: "shipment" },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  cars: { type: [mongoose.Schema.Types.ObjectId],ref:'cars', required: true },
+  shipmentId: {type: mongoose.Schema.Types.ObjectId, ref: "shipment" },
   status: { type: Boolean, default:false}
 });
 
