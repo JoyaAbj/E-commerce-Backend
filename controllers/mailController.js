21a922ffbea26d3e8eaaf0bdd4e6012abf70b2af
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-12-12 15:10:51 +0200

    some edits on the email

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,4 +1,6 @@
 const nodemailer=require('nodemailer');
+const send=(req,res)=>{
+const {subject,message,name}=req.body;
 const transporter=nodemailer.createTransport({
     service:'gmail',
     pool: true,
@@ -6,18 +8,19 @@ const transporter=nodemailer.createTransport({
   port: 465,
   secure: true ,
     auth:{
-
         user:'hadimortada1245@gmail.com',
         pass:'f n e i l x z q f l y r s g b e' 
     }
 });
 const options={
-    from:'hadimortada1245@gmail.com',
-    to:'mohammad.sleimane@isae.edu.lb',
-    subject:"Dear mhmd",
-    text:'You bought a mercedes car from Hadi cars hub and you choose Zahle as a location to receive it,It takes time to be there maybe 3 days.' 
+    from:'DriveEpic',
+    to:'joyaaboujaoude627@gmail.com',
+    subject:"Dear Joya",
+    text:`${message}` 
 }
 transporter.sendMail(options,(error,info)=>{
     if(error)console.log(error);
     else console.log(info.response);
 })
+}
+module.exports={send}
