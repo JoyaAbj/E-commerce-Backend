cb90e6e885e039e8c5aa15162ad1bb8f1d2e2110
Mhmd Sleimane <mohammad.sleimane@isae.edu.lb>
2023-12-15 09:27:59 +0200

    updating on review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema({
   fullName: {type: String, required:true},
-  Email: { type: String, required:true},
   comment:{type:String , required:true},
   rating:{type:Number, required:true}
 },{ timestamps: true });
