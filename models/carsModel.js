0d3e872f99523be17d25584babca167c37f4209b
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-12-20 11:03:13 +0200

    addition of space

--- models/carsModel.js
+++ models/carsModel.js
@@ -1,20 +1,23 @@
 const { timeStamp } = require("console");
 const mongoose = require("mongoose");
 
-const carsSchema = new mongoose.Schema({
-  carName: { type: String, required: true },
-  company: { type: String, required: true },
-  type: { type: String, required: true },
-  description: { type: String, required: true },
-  initialPrice: { type: Number, required: true },
-  sellingPrice: { type: Number, required: true },
-  TVA:{type:Number, required:true},
-  discount:{type: Number},
-  quantity:{type: Number,required:true},
-  file:{type:Array,required:true}, // it could also be of type:String , Both cases tested
-  DOR:{type:String,required:true},
-  color:{type:String, required:true}
-},{ timestamps: true });
+const carsSchema = new mongoose.Schema(
+  {
+    carName: { type: String, required: true },
+    company: { type: String, required: true },
+    type: { type: String, required: true },
+    description: { type: String, required: true },
+    initialPrice: { type: Number, required: true },
+    sellingPrice: { type: Number, required: true },
+    TVA: { type: Number, required: true },
+    discount: { type: Number },
+    quantity: { type: Number, required: true },
+    files: { type: Array, required: true }, // it could also be of type:String , Both cases tested
+    DOR: { type: String, required: true },
+    color: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const Cars = mongoose.model("cars", carsSchema);
 module.exports = Cars;
